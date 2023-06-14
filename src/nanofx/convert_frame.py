29b03b13db8569d91080416e7431a0bd04a6d239
Allen Guo <guosonglin001@gmail.com>
2023-06-14 17:43:08 +0000

    wip

--- src/nanofx/convert_frame.py
+++ src/nanofx/convert_frame.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import dataclasses
 import types
 
 from typing import Any, Callable
@@ -7,27 +8,57 @@ from typing import Any, Callable
 from .utils import print_bytecode
 
 
-def has_tensor_in_frame(frame: types.FrameType) -> bool:
+@dataclasses.dataclass
+class GuardedCode:
+    code: types.CodeType
+
+
+def skip_frame(frame: types.FrameType) -> bool:
     import paddle
 
     # NOTE: skip paddle internal code
     if frame.f_code.co_filename.endswith('paddle/fluid/dygraph/math_op_patch.py'):
-        return False
-    if frame.f_code.co_name == 'in_dygraph_mode':
-        return False
+        return True
+    elif frame.f_code.co_filename.endswith('paddle/fluid/framework.py'):
+        return True
+    elif frame.f_code.co_name == 'in_dygraph_mode':
+        return True
 
     for v in frame.f_locals.values():
         if isinstance(v, paddle.Tensor):
-            return True
+            return False
 
-    return False
+    return True
 
 
-def convert_frame(frame: types.FrameType, compiler_fn: Callable) -> Any:
+def convert_frame(frame: types.FrameType, compiler: Callable) -> Any:
+    from .eval_frame import disable
+
+    if skip_frame(frame):
+        return None
+
+    print(f"convert_frame: {frame}")
     code = frame.f_code
 
+    compiled_fn = compiler(code, None)
+    compiled_fn = disable(compiled_fn)
+
+    # TODO: add CALL_FUNCTION to compiled_fn
+    out_code = compiled_fn.__code__
+
     print_bytecode(
-        "RAW BYTECODE", code.co_name, code.co_filename, code.co_firstlineno, code
+        "RAW BYTECODE",
+        code.co_name,
+        code.co_filename,
+        code.co_firstlineno,
+        code,
+    )
+    print_bytecode(
+        "NEW BYTECODE",
+        code.co_name,
+        code.co_filename,
+        code.co_firstlineno,
+        out_code,
     )
 
     # debug, no trace
