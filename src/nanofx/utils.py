04f317fc1b3021f949fa59130600959800c64437
Allen Guo <guosonglin001@gmail.com>
2023-06-15 14:25:17 +0000

    wip

--- src/nanofx/utils.py
+++ src/nanofx/utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dis
+import types
 
 
 def format_bytecode(prefix, name, filename, line_no, code):
@@ -9,3 +10,7 @@ def format_bytecode(prefix, name, filename, line_no, code):
 
 def print_bytecode(prefix, name, filename, line_no, code):
     print(format_bytecode(prefix, name, filename, line_no, code))
+
+
+def print_code(code: types.CodeType, prefix='CODE'):
+    print_bytecode(prefix, code.co_name, code.co_filename, code.co_firstlineno, code)
