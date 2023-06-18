c38f2165c543299ee3e21ecedb2834ed13ac4be7
Allen Guo <guosonglin001@gmail.com>
2023-06-18 04:56:31 +0000

    wip

--- src/nanofx/codegen.py
+++ src/nanofx/codegen.py
@@ -3,12 +3,24 @@ from __future__ import annotations
 import collections
 import sys
 
+from functools import lru_cache
 from typing import TYPE_CHECKING
 
 from .bytecode_transformation import *  # noqa
+from .paddle_utils import TensorType
 
 if TYPE_CHECKING:
-    pass
+    from .ceval import SymVar
+
+
+@lru_cache(32)
+def rot_n_helper(n):
+    assert n > 1
+    vars = [f"v{i}" for i in range(n)]
+    rotated = reversed(vars[-1:] + vars[:-1])
+    fn = eval(f"lambda {','.join(vars)}: ({','.join(rotated)})")
+    fn.__name__ = f"rot_{n}_helper"
+    return fn
 
 
 class PyCodegen:
@@ -17,105 +29,62 @@ class PyCodegen:
         tx=None,
         graph_output_var: str = None,
     ):
-        self.graph_output_var = graph_output_var
-
         self.tx = tx
-        # self.code_options = self.tx.output.code_options
-        # self.cell_and_freevars = self.tx.cell_and_freevars
-        # self.new_var = self.tx.output.new_var
+        self.code_options = self.tx.output.code_options
+
+        self.graph_output_var = graph_output_var
 
         self.top_of_stack = None
         self.graph_outputs = collections.OrderedDict()
-        self._output: list[Instruction] = []
+        self.instructions: list[Instruction] = []
 
-    def graph_output_vars(self):
-        return [x.variable for x in self.graph_outputs.values()]
-
-    def __call__(self, value):
+    def call_one(self, value: SymVar):
         """Generate code such that top-of-stack (TOS) is set to value."""
-
-        if isinstance(value, Source):
-            self._output.extend(value.reconstruct(self))
-            self.clear_tos()
-            return
-
-        assert isinstance(value, VariableTracker)
-        output = self._output
+        output = self.instructions
         graph_outputs = self.graph_outputs
 
         if self.top_of_stack is value:
             output.append(create_dup_top())
             return
 
-        if value.is_python_constant() and is_safe_constant(value.as_python_constant()):
-            output.append(self.create_load_const(value.as_python_constant()))
-        elif isinstance(
-            value,
-            (
-                TensorVariable,
-                SymNodeVariable,
-                TensorWithTFOverrideVariable,
-                UnspecializedPythonVariable,
-                NumpyNdarrayVariable,
-            ),
-        ):
-            if isinstance(value, TensorWithTFOverrideVariable):
-                # unwrap back to tensor
-                value = value.tensor_variable
-            graph_outputs_key = id(value.proxy)
+        if isinstance(value.vtype, (TensorType,)):
+            graph_outputs_key = id(value)
             if graph_outputs_key not in graph_outputs:
-                graph_outputs[graph_outputs_key] = GraphOutputEntry(
-                    len(graph_outputs), value
-                )
-            else:
-                graph_outputs[graph_outputs_key].merge(value)
-            if isinstance(value, NumpyNdarrayVariable):
-                self.load_import_from(utils.__name__, "to_numpy_helper")
+                graph_outputs[graph_outputs_key] = value
+
             output.append(self.create_load(self.graph_output_var))
-            output.append(
-                self._create_load_const(graph_outputs[graph_outputs_key].index)
-            )
+            # TODO: rm hardcode
+            output.append(self.create_load_const(0))
             output.append(create_instruction("BINARY_SUBSCR"))
-            if isinstance(value, NumpyNdarrayVariable):
-                output.extend(create_call_function(1, False))
-            elif isinstance(value, UnspecializedPythonVariable) and value.need_unwrap:
-                output.extend(
-                    [self.create_load_attr("item")] + create_call_function(0, True)
-                )
         else:
-            try:
-                output.extend(value.reconstruct(self))
-            except NotImplementedError:
-                unimplemented(f"reconstruct: {value}")
+            # TODO: support container types
+            raise NotImplementedError(f"unsupported type: {type(value)}")
 
         self.top_of_stack = value
 
-    def foreach(self, items):
-        for i in items:
-            self(i)
+    def call(self, vars: list[SymVar]):
+        for var in vars:
+            self.call_one(var)
 
     def clear_tos(self):
         self.top_of_stack = None
 
     def append_output(self, inst):
         assert isinstance(inst, Instruction)
-        self._output.append(inst)
+        self.instructions.append(inst)
         self.clear_tos()
 
     def extend_output(self, insts):
         assert all(isinstance(x, Instruction) for x in insts)
-        self._output.extend(insts)
+        self.instructions.extend(insts)
         self.clear_tos()
 
-    def get_instructions(self):
-        return self._output
-
     def create_load(self, name):
-        assert name in self.code_options["co_varnames"], f"{name} missing"
+        # assert name in self.code_options["co_varnames"], f"{name} missing"
         return create_instruction("LOAD_FAST", argval=name)
 
     def create_store(self, name):
-        assert name in self.code_options["co_varnames"]
+        # assert name in self.code_options["co_varnames"]
         return create_instruction("STORE_FAST", argval=name)
 
     def create_load_global(self, name, push_null):
@@ -123,12 +92,7 @@ class PyCodegen:
         return create_load_global(name, push_null)
 
     def create_load_const(self, value):
-        return self._create_load_const(value)
-
-    def _create_load_const(self, value):
         return create_instruction("LOAD_CONST", argval=value)
-
-    create_load_output = _create_load_const
 
     def create_load_attr(self, name):
         if name not in self.code_options["co_names"]:
@@ -156,7 +120,7 @@ class PyCodegen:
             # desired rotate bytecode doesn't exist, generate equivalent bytecode
             return [
                 create_instruction("BUILD_TUPLE", arg=n),
-                self._create_load_const(rot_n_helper(n)),
+                self.create_load_output(rot_n_helper(n)),
                 *create_rot_n(2),
                 create_instruction("CALL_FUNCTION_EX", arg=0),
                 create_instruction("UNPACK_SEQUENCE", arg=n),
@@ -192,6 +156,3 @@ class PyCodegen:
             arg=0,
         )
         self.extend_output([call_function])
-
-    def create_instruction(self, opname, arg=None, argval=None):
-        return create_instruction(opname, arg=arg, argval=argval)
--- src/nanofx/utils.py
+++ src/nanofx/utils.py
@@ -17,5 +17,16 @@ def log_code(code: types.CodeType, prefix=''):
     log_bytecode(prefix, code.co_name, code.co_filename, code.co_firstlineno, code)
 
 
+def log_instructions(instructions: list[dis.Instruction]):
+    def format_instruction(inst: dis.Instruction):
+        if inst.arg is None:
+            return f"\t{inst.opname: <25} {'': <2} ({inst.argval})"
+        else:
+            return f"\t{inst.opname: <25} {inst.arg: <2} ({inst.argval})"
+
+    for inst in instructions:
+        logging.debug(format_instruction(inst))
+
+
 def get_instructions(code: types.CodeType):
     return list(dis.get_instructions(code))
