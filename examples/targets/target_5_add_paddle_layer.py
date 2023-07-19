9b6d91c73f4161e60e6cdd890367c6998fc23470
Allen Guo <guosonglin001@gmail.com>
2023-07-19 16:10:41 +0000

    wip

---
+++ examples/targets/target_5_add_paddle_layer.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import logging
+
+# ignore DeprecationWarning from `pkg_resources`
+logging.captureWarnings(True)
+
+import paddle
+import paddle._C_ops
+import paddle.nn
+
+import nanofx
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+# logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+paddle.seed(0)
+
+
+def my_compiler(gl, example_inputs=None):
+    print("my_compiler() called with FX graph:")
+
+    gl.print_tabular()
+
+    # gl.graph.print_tabular()
+    # return gl.forward
+
+    # dummy_print
+    def dummy_print(*args, **kwargs):
+        print("==== dummy_print: ")
+        return (args[0],)
+
+    return dummy_print
+
+
+class MyLayer(paddle.nn.Layer):
+    def __init__(self):
+        super().__init__()
+        self._linear = paddle.nn.Linear(1, 1)
+        self._dropout = paddle.nn.Dropout(p=0.5)
+
+    def forward(self, input):
+        temp = self._linear(input)
+        temp = self._dropout(temp)
+        return temp
+
+
+mylayer = MyLayer()
+mylayer.eval()
+
+mylayer = nanofx.optimize(my_compiler)(mylayer)
+
+x = paddle.randn([10, 1], 'float32')
+out = mylayer(x)
+print("res = ", out)
